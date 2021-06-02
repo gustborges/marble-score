da61139defef200b986891c6e3f7b5379f14a5d2
Gustavo Borges <gustborges@gmail.com>
2021-06-02 12:49:23 -0300

    Created the app template with devise and bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :email, presence: true, uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def admin?
+    admin == true
+  end
 end
